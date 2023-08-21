555f024a6eeb022ce7f09339892ad1f7c41fcb06
m-skowronski <mskowronski547@gmail.com>
2023-08-21 22:15:00 +0200

    RPM-1 initialize Spring project

---
+++ src/test/java/com/mskowronski/rentalproductsmanager/RentalProductsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mskowronski.rentalproductsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalProductsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
